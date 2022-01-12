305f4558241f23e75988652e1590c6f4cede67ee
davidfdzmorilla <davidfdzmorilla@gmail.com>
2022-01-12 14:19:09 +0100

    mimetypes compare

--- controllers/houses/createHouse.js
+++ controllers/houses/createHouse.js
@@ -1,28 +1,86 @@
+const fs = require('fs-extra')
 const { houseSchema } = require('../../validators')
 const { housesRepository } = require('../../repository/')
+const uploads = require('../../shared/uploads')
+
+
+const { MAX_IMAGE_SIZE_IN_BYTES, UPLOADS_PATH } =  process.env
+
+
+// TODO 
+//  En la función si es valido el formato hay que comparar los dos arrays
 
 const createHouse = async (req, res) => {
     let insertId
-    const house = req.body
+    let house = req.body
+    
+    // if(!req.files || !req.files.picture) {
+    //     res.status(400)
+    //     res.end('[picture] is required')
+    //     return
+    // }
+    const pictures = [
+        {
+            name: 'autumn-g820e3a0f3_1920.jpg',
+            data: '<Buffer ff d8 ff e0 00 10 4a 46 49 46 00 01 01 00 00 01 00 01 00 00 ff db 00 43 00 05 03 04 04 04 03 05 04 04 04 05 05 05 06 07 0c 08 07 07 07 07 0f 0b 0b 09 ... 729935 more bytes>',
+            size: 729985,
+            encoding: '7bit',
+            tempFilePath: '',
+            truncated: false,
+            mimetype: 'image/jpg',
+            md5: '39a8999e24444189abe284da4763541a',
+            mv: '[Function: mv]'
+        },
+        {
+            name: 'slon-con-cocina.jpg',
+            data: '<Buffer ff d8 ff e0 00 10 4a 46 49 46 00 01 01 00 00 01 00 01 00 00 ff db 00 43 00 05 03 04 04 04 03 05 04 04 04 05 05 05 06 07 0c 08 07 07 07 07 0f 0b 0b 09 ... 729935 more bytes>',
+            size: 729985,
+            encoding: '7bit',
+            tempFilePath: '',
+            truncated: false,
+            mimetype: 'image/jpeg',
+            md5: '39a8999e24444189abe284da4763541a',
+            mv: '[Function: mv]'
+        }
+    ]
+
+    house = { ...house, pictures: pictures}
 
     try {
-        await houseSchema.validateAsync(house)
-        if(house.pictures.length < 1) throw new Error ('Must upload at least one picture')
-    } catch (error) {
-        res.status(401)
-        res.end(error.message)
+            await houseSchema.validateAsync(house)
+            if(house.pictures.length < 2) throw new Error ('Must upload at least two pictures')
+        } catch (error) {
+                res.status(401)
+                res.end(error.message)
+                return
+            }
+            if (!uploads.areValidImagesSize(pictures)) {
+        res.status(400)
+        res.end(`Picture size should be less than ${MAX_IMAGE_SIZE_IN_BYTES / 1000000} Mb`)
         return
     }
-    try {
-
-        insertId = await housesRepository.saveHouse({ ...house, ownerId: req.user.id })
-
-    } catch (error) {
-        res.status(500)
-        res.end(error.message)
+    
+    if (!uploads.areValidImagesMimeType(pictures)) {
+        res.status(400)
+        res.end('No valid mime type')
         return
     }
-    res.status(201)
+    // const pictureName = uploads.createImageName(uploads.getExtensionFromMimetype(pictures.mimetype))
+    // const picturesUrl = `${UPLOADS_PATH}/${pictureName}`
+
+    // try {
+        
+    //         insertId = await housesRepository.saveHouse({ ...house, ownerId: req.user.id, pictures: picturesUrl })
+    //         console.log(house)
+    //     } catch (error) {
+    //             res.status(500)
+    //             res.end(error.message)
+    //             return
+    //         }
+    //         fs.ensureDir(UPLOADS_PATH)
+    //         pictures.mv(`${UPLOADS_PATH}/${pictureName}`)
+    // res.status(201)
+    // res.send(`Created new house`)
     res.send(`Created new house with id ${insertId}`)
 }
 
---
+++ shared/uploads.js
@@ -0,0 +1,47 @@
+const { MAX_IMAGE_SIZE_IN_BYTES, UPLOADS_PATH } =  process.env
+
+const ALLOWED_MIMETYPES = ['image/jpg', 'image/jpeg', 'image/png']
+
+const crypto = require('crypto')
+
+
+const areValidImagesSize = (picturesData) => {
+  const areValid = picturesData.find(picture => picture.size >= MAX_IMAGE_SIZE_IN_BYTES)
+  if (areValid) return false
+  return true
+}
+
+  const areValidImagesMimeType = (picturesData) => {
+    let mimetypesData = []
+    picturesData.map(function(picture) {
+      let mimetype =  picture.mimetype.toLowerCase()
+      mimetype = mimetype.split('/')[1]
+      mimetypesData.push(mimetype)
+    })
+    const formatedValidsMimetsypes = []
+    ALLOWED_MIMETYPES.map(function(validMimetype) {
+      let mimetype =  validMimetype.toLowerCase()
+      mimetype = mimetype.split('/')[1]
+      formatedValidsMimetsypes.push(mimetype)
+    })
+    console.log(areValids)
+    console.log(formatedValidsMimetsypes)
+    // return ALLOWED_MIMETYPES.includes(mimetype.toLowerCase())
+}
+
+
+  const removeFile = async (fileName) => {
+    fs.remove(`${UPLOADS_PATH}/${fileName}`)
+}
+
+  const createImageName = (extension) => {
+    const randomHash = crypto.randomBytes(15).toString('hex')
+    return `${randomHash}.${extension}`
+}
+
+module.exports = {
+    areValidImagesSize,
+    areValidImagesMimeType,
+    removeFile,
+    createImageName
+}
